7760749ff9cb665932a37ae6e6ed636023a50b1e
Brian Keene <bkeene@nd.edu>
2016-09-08 19:38:32 -0500

    substantial progress. Iteratively creates nested notebook objects and displays correctly.

--- GUI_Template.py
+++ GUI_Template.py
@@ -7,6 +7,7 @@
 # graphical user interfaces; primarily intended to be used as a simplified front-end of wx.Widgets,
 # allow for easy setup of dynamic hiding that might involve cross-communication between objects of
 # assorted placement in the hierarchy of parent-child objects.
+# Allows for an OOP creation of a GUI, with emphasis on easy modification in the accompanying script.
 
 ######
 #
@@ -20,176 +21,211 @@ from wx.lib.pubsub import pub
 # global dictionary in which we store data
 myDict = {}
 
-class Frame(wx.Frame):
+class wxFrame(wx.Frame):
     # note to others: we pass another class (an instance of MainFrame) to this wx.Frame derived class;
     # the ambiguity of parent in the class __init__ vs the wx.Frame.__init__ is due to parent in the
     # wx.Frame.__init__ function being a /keyword/ argument, rather than a python convention, as is used
     # in the class __init__ funciton.  The wx.Frame.__init__ parent argument /must/ be a wx.Window object,
     # or simply value "None", which is what we usually use
-    def __init__(self,parent):
-        wx.Frame.__init__(self,parent=parent._windowObj,title = parent._title, size = parent._size)
-        self.Show()
-
+    def __init__(self,sibling):
+        wx.Frame.__init__(self,parent=sibling._windowObj,title = sibling._title, size = sibling._size)
 
 # we define our own MainFrame() class, because we don't instantly want to create an actual wx.Frame object yet
-class MainFrame:
-  _register = []
-    # implicit argument self
-    # parent: typically None, but if a frame is spawned dynamically it may be useful to pass the relevant object
-    # title: string displayed at the top of the frame (the name)
-    # size: integer tuple (e.g., (100,100)) specifying the size of the frame
-  def __init__(self, parent, title, size, **kwargs):
-      self._parent = parent;
-      self._title = title;
-      self._size = size;
-      self._windowObj = kwargs.get("windowObj",None)
+class Frame:
+
+    # a static class object we can access using MainFrame._register[index] - we don't access this via an instance of
+    # the class; we can also iterate over it, looking for objects with
+    _register = []
+    _typeName = "Frame"
+
+      # implicit argument self
+      # parent: typically None, but if a frame is spawned dynamically it may be useful to pass the relevant object
+      # title: string displayed at the top of the frame (the name)
+      # size: integer tuple (e.g., (100,100)) specifying the size of the frame in pixels
+    def __init__(self, parent, title, size, **kwargs):
+        self._parent = parent;
+        self._title = title;
+        self._size = size;
+        self._windowObj = kwargs.get("windowObj",None)
+
+        # an instance variable holding other instances that are children of this instance
+        self._children = []
 
 
-  def initFrame(self):
+    def initObj(self):
 
-      # make an instance of the frame, that is an derived class of the wx.Frame class
-      self._frame = Frame(self)
+        # make an instance of the frame, that is an derived class of the wx.Frame class
+        self._obj = wxFrame(self)
+        Frame._register.append(self)
+
+        # iterate over this instance's children and initialize them.
+        for obj in self._children:
+            obj.initObj();
+        self._obj.Show()
+
 
 # this class will be used as the parent panel for any notebook-type pages;
 # likely that these Notebook objects will be the direct children of the frame, rather than an
 # object of the Panel class
+# or is it an object of the notebook class?
+#class wxNotebook:
+#
+#    # an implicit self argument
+#    # and the parent instance to which this notebook will be added
+#    def __init__(self,sibling):
+#        self._obj = wx.Notebook(sibling._parent._obj);
+#
+#        self._children = [];
+#        for index, obj in enumerate(sibling._children):
+#            # call the init() methods of the child objects;
+#            # and then we want to append the wx instances of the child objects
+#            # after that, we want to head to
+#            obj.initObj();
+#            self._children.append(obj._obj)
+#            self._obj.AddPage(self._children[index], obj._name)
 
-# TODO: this needs to be refined, heavily
-class Notebook(wx.Panel):
+
+# our notebook class that collates information before making a wx.Notebook notebook
+class Notebook:
     # the implicit self argument
     # parent panel object
     # the pages to be added to this notebook
     # and the names of the pages
-  def __init__(self,parent,pages):
-    # instantiate the notebook
-    thisNotebook = wx.Notebook(self, parent)
-    self.listOfPageObjs = []
-    self.pageObjs = []
+    _register = []
+    _typeName = "Notebook"
 
-    for index, item in enumerate(pages):
-      self.listOfPageObjs.append(str(item))
-      self.pageObjs.append(item(thisNotebook))
+    def __init__(self,parent, **kwargs):
+        # instantiate the notebook
+        self._parent = parent;
+        # an instance variable holding other instances that are children of this instance
+        self._children = [];
+        self._pages = [];
+        # append this instance to a list belonging to the parent, so that the parent knows
+        parent._children.append(self);
 
-      ## need their names here
-      # we should be able to obtain them via a wx() method
-      thisNotebook.AddPage(self.pageObjs[index],objName)
+    def initObj(self):
 
-    # add a sizer to the notebook
-    thisSizer=wx.BoxSizer()
-    thisSizer.Add(thisNotebook,1,wx.EXPAND)
-    self.SetSizer(thisSizer)
+        # our wxNotebook method initiates the instantation of the self._children objects
+        self._obj = wx.Notebook(self._parent._obj)
+        for index, item in enumerate(self._children):
+            item.initObj();
+            self._pages.append(item._obj);
+            self._obj.AddPage(self._pages[index], item._name);
+        self.NBSizer = wx.BoxSizer();
+        self.NBSizer.Add(self._obj, 1, wx.EXPAND)
+        self._parent._obj.SetSizer(self.NBSizer)
+        Notebook._register.append(self)
 
-    self.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGING, self.OnPageChanging)
-  # we can define this dynamically (per-instance of the object)
-  # and outside of the class
-  def customBehavior():
-    pass
+    def customBehavior():
+        pass
 
-  def OnPageChanging(self,event):
-    oldPage = event.GetOldSelection()
-    newPage = event.GetSelection()
+    # i think this has to be incorporated in the wxNotebook class, rather than here;
+    def OnPageChanging(self,event):
+        oldPage = event.GetOldSelection()
+        newPage = event.GetSelection()
 
-    customBehavior()
+        customBehavior()
 
-class Panel(wx.Panel):
-    def __init__(self,parent,wxObj):
-        # again, in the wx.Panel.__init__ function call, parent is a keyword argument that takes a wx object
-        wx.Panel.__init__(self,parent=wxObj);
+class wxPanel(wx.Panel):
+    def __init__(self,sibling):
+        print sibling._name
+        wx.Panel.__init__(self,parent=sibling._parent._obj);
+        self.grid = wx.GridBagSizer(hgap=5,vgap=5);
+        self.SetSizer(self.grid);
+
+        # call the init methods of the objects, which then places wxWidget objects in the self._widgets variable for
+        # each Widget class instance
+        for obj in sibling._children:
+            if obj._typeName == "Widget":
+                obj.init();
+                self.grid.Add(obj._widget, pos=obj._pos, span=obj._span)
+                # call the self.Bind() functionality here
 
 # in this class, we collate all the information we'll need to make a well-defined wx.Panel object
-class BasePanel:
+class Panel:
+    # what do we require from the user to instantiate a base panel object?
+    # make an iterable list of panel instances; make sure methods only access this /after/
+    # the main frame has added all objects (i.e., at the end of th user's GUI script!)
+    _register = []
 
-  def __init__(self,parent):
-    # on initialization, the panel should be initialized ...
-    wx.Panel.__init__(self,parent);
+    # all instances of this class have the _typeName = "Panel"
+    _typeName = "Panel"
 
-    # a grid object, on which widgets will be placed
-    self.grid = wx.GridBagSizer(hgap=5,vgap=5);
+    def __init__(self, parent,**kwargs):
 
-    # we record whether this panel is a notebook; whether it has a friend ;
-    # whether it has a parent; whether it has a child; whether it has widgets;
+        # a list of widget objects, from our widgets class, that identify this panel as their parent panel
+        # note that we do /not/ need more information, as this has the instanced objects; we can call their methods
+        # directly from here! Very convenient.
+        self._widgets = [];
 
-    # note that all panels have a parent; top level panels have the frame as their parent object,
-    # which in turn has the window as its parent
-    self._isNotebook = False;
-    self._hasFriend = False;
-    self._hasParent = True;
-    self._hasChild = False;
-    self._hasWidgets = False;
+        # panel must have parent object on which it is displayed
+        self._parent = parent;
 
-    # a given panel may be a member of one notebook;
-    # a given panel may have multiple siblings;
-    # a given panel may have one parent;
-    # a given panel may have multiple children;
-    self._notebookName = [];
-    self._friends = [];
+        # a list of the instances that have this instance of the Panel class as their parent
+        self._children = []
+        parent._children.append(self);
 
-    # TODO: pseudocode; change this
-    #self._parent = wx.GetName(parent);
-    # end todo
+        self._name = kwargs.get("name",None)
 
-    self._children = [];
-    self._widgets = [];
+    def initObj(self):
+        # we initialize the panel, which then refers to all of the panel's widgets' methods for their instantiation
+        self._obj = wxPanel(self);
 
+        # append this instance to the class register, so that we may iterate over the class instances if needed
+        Panel._register.append(self);
 
-  def addWidget(widget):
-    # when adding a widget, we consider the grid position it will be added to, as well
-    # as the flags which will be implemented when placing the widget on the grid
+        # iterate over self._children, and initialize objects that are /not/ of the type widget; these will
+        # be initialized in the wxPanel class!
+        for obj in self._children:
+            if (obj._typeName != "Widget"):
+                obj.initObj()
 
-    #first, we need to get the information from the widget
+    def deleteWidget():
+        pass
 
-    # TODO: un-pseudocode this;
-    #some pseudocode of what we need from the widget when we instantiate it
-    #widgetName = wx.GetName(widget);
-    #widgetID = wx.GetID();
-    #widgetCoords = wx.GetCoords(widget);
-    #widgetSpan = wx.GetSpan(widget);
-    #widgetFlags = wx.GetFlags(widget);
+    def bindToFunction():
+         # ehhhh... we might have already done this in the widget class. could be better that way.
+        pass
 
-    # should the widget already be bound to a function? consider this (TODO)
-    #self.grid.Add(widget.
-    pass
-
-    # destroy an object of type Widget
-  def deleteWidget():
-    pass
-
-  def bindToFunction():
-    # ehhhh... we might have already done this in the widget class. could be better that way.
-    pass
-
-# TODO: consider, what other information is general to all widgets? are there any objects here now
-# that might be considered extraneous to the majority of them? reduce waste
 class Widget:
-  def __init__(self,name,coords,widgetType,span=(1,1),parent=None,label=None,labelPosition=None,**kwargs):
-    # note that we use **kwargs to pass in information that may be specific to certain type
-    # of widget; e.g., text widget vs button vs ... etc.
-    # **kwargs is a list of KeyWord ARGumentS (kwargs)  of arbitrary length
-    # note that, by default, there is no label (and no label position <(int,int)> provided
-    self._name = name; #string
-    self._coords = coords; #tuple of coords: "(integer, integer)"
-    self._widgetType = widgetType; # button, textwidget, label, etc.
-    self._parent = parent;
-    # default behavior of span is (1,1) if not specified;
-    self._span = span
+    _register = []
+    _typeName = "Widget"
 
-    if label:
-      self.label = label
-      try:
-        self.labelPosition = labelPosition
-      except:
-        print "Error: label %s specified, but no label position tuple (int,int) given! " %label
 
-      #  if self._widgetType is "staticText":
-      pass
-    elif self._widgetType is "button":
-      pass
-    elif self._widgetType is "textWidget":
-      pass
+    def __init__(self,name,coords,widgetType,span=(1,1),parent=None,label=None,labelPosition=None,**kwargs):
+        # note that we use **kwargs to pass in information that may be specific to certain type
+        # of widget; e.g., text widget vs button vs ... etc.
+        # **kwargs is a list of KeyWord ARGumentS (kwargs)  of arbitrary length
+        # note that, by default, there is no label (and no label position <(int,int)> provided
+        self._name = name; #string
+        self._coords = coords; #tuple of coords: "(integer, integer)"
+        self._widgetType = widgetType; # button, textwidget, label, etc.
+        self._parent = parent;
+        # default behavior of span is (1,1) if not specified;
+        self._span = span
 
-  def instantiateWidget(self):
-    self._parent.addWidget(self)
+        # a buffer value - holds the data immediately prior to the widget's most recent interaction
+        # for choice widgets, this will be a complete value; for textwidgets, it will be string = str[:-2]
+        self._buffer = ""
 
-  def makeWidget(self):
-    pass
+        if label:
+            self.label = label
+            try:
+                self.labelPosition = labelPosition
+            except:
+                print "Error: label %s specified, but no label position tuple (int,int) given! " %label
+
+            #  if self._widgetType is "staticText":
+            pass
+
+        elif self._widgetType is "button":
+            pass
+        elif self._widgetType is "textWidget":
+            pass
+
+    def instantiateWidget(self):
+        self._parent.addWidget(self)
+
+    def makeWidget(self):
+        pass
 
--- test.py
+++ test.py
@@ -1,36 +1,134 @@
-# proof of concept importing the template and setting up the GUI in a process
-# similar to lammps input file script
-
+# import the resources from GUI_Template
 from GUI_Template import *
 
-# utf-encoded angstroms unit, for use in labels on the GUI
+######################################################################################
+# SECTION 1: Creation of the main frame object and accompanying panels
+######################################################################################
+# MainFrame: the top level window that holds all objects
+# TopPanel: the top level panel that we can put other widgets/notebooks etc on
+# TopNotebook: holds the other panels in the style that we want
+# PanelOne: the "Basic Information" panel
+# PanelTwo: the "Interaction Parameters" panel
+# PanelThree: the "Probabilities Information" panel
+# PanelFour: the "File Handling" panel
+#
+######################################################################################
 
-# the size of the main frame - adjust here to change size of the GUI on the screen;
-# must be an integer tuple
-size = (640,480);
-# the main frame is a bet special; we can't actually create the object until the app is finished
-thisFrame = MainFrame(None,"Hello World",size);
+# change 'size' to any tuple of integers; this variable denotes the size, in
+# pixels, that the frame (GUI) will appear on the user's screen
+size = (900,620);
+MainFrame = Frame(None,"Cassandra Input File Editor v1.2",size);
+
+TopPanel = Panel(MainFrame);
+TopNotebook = Notebook(TopPanel);
+
+PanelOne = Panel(TopNotebook,name="Basic Information");
+PanelTwo = Panel(TopNotebook,name="Interaction Parameters");
+PanelThree = Panel(TopNotebook,name="Probabilities Information");
+PanelFour = Panel(TopNotebook,name="File Handling");
+
+######################################################################################
 
 
-# let's try adding a simple panel with the first few widgets;
-# first, we'll need to make the panel, then the notebook, then the other panels...
-# and then we place the widgets on that bottom panel
-
-# let's make some pseudocode; ideally, the only thing the next person maintaining should have to do
-# is provide arguments (including the variable name of the parent object) and some preliminary information
 
 
 
-#topLevelPanel = Panel(*args)
-#topLevelNotebook = Notebook(*args)
-#PanelOneA = Panel(*args)
-#fakeWidget
-# don't we need names for all the widgets though? I think so..
+######################################################################################
+# SECTION 2: Creation of subnotebooks
+######################################################################################
+# The main panels have smaller sections in them that allow for easier navigation
+# of the GUI.  Here, we create notebooks to hold these sections, and provide
+# names for the panels that will then appear on the bar atop the notebooks or
+# panels.
+#
+# PanelOneNotebook: holds subpanels of "Basic Information"
+# PanelTwoNotebook: holds subpanels of "Interaction Parameters"
+# PanelThreeNotebook: holds subpanels of "Probabilities Information"
+# PanelFourNotebook: holds subpanels of "File Handling"
+#
+# PanelOnePageOne: a subpanel of PanelOneNotebook, titled "Page 1"
+# PanelOnePageTwo: a subpanel of PanelOneNotebook, titled "Page 2"
+# and etc. for the remaining notebooks and subpanels.
+#######################################################################################
+
+# create the notebooks that will hold the subpanels
+PanelOneNotebook = Notebook(PanelOne);
+PanelTwoNotebook = Notebook(PanelTwo);
+PanelThreeNotebook = Notebook(PanelThree);
+PanelFourNotebook = Notebook(PanelFour);
+
+## the pages of the panel one notebook, "Basic Information"
+PanelOnePageOne = Panel(PanelOneNotebook,name="Page 1")
+PanelOnePageTwo = Panel(PanelOneNotebook,name="Page 2")
+
+# the pages of the panel two notebook, "Interaction Parameters"
+PanelTwoIntramolecular = Panel(PanelTwoNotebook,name="Intramolecular")
+PanelTwoIntermolecular = Panel(PanelTwoNotebook,name="Intermolecular")
+
+# the pages of the panel three notebook, "Probabilities Information"
+PanelThreeTranslation = Panel(PanelThreeNotebook,name="Translation")
+PanelThreeRotation = Panel(PanelThreeNotebook,name="Rotation")
+PanelThreeRegrowth = Panel(PanelThreeNotebook,name="Regrowth")
+PanelThreeVolume = Panel(PanelThreeNotebook,name="Volume")
+PanelThreeInsertion = Panel(PanelThreeNotebook,name="Insertion")
+PanelThreeSwap = Panel(PanelThreeNotebook,name="Swap")
+
+# the pages of the panel four notebook, "File Handling"
+PanelFourMoleculeFiles = Panel(PanelFourNotebook,name="Molecule Files")
+PanelFourFragmentFiles = Panel(PanelFourNotebook,name="Fragment Files")
+PanelFourInputFile = Panel(PanelFourNotebook,name="Input File")
+PanelFourOutputFile = Panel(PanelFourNotebook,name="Output File")
+
+#######################################################################################
+
+
+
+
+
+
+
+
+######################################################################################
+# SECTION 3.1: Addition of widgets to PanelOnePageOne
+######################################################################################
+# PanelOnePageOne is the panel to which the graphical user interface opens.
+# This panel is special in that it incorporates some navigational restrictions,
+# based on the current values of the widgets on the page! These restrictions are
+# enforced by (syntax)
+#
+#
+#
+#
+#
+#
+#
+######################################################################################
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+######################################################################################
 
 # initiate the event loop, and instruct the main frame to show!
+# this causes a cascade of instantiations - at this point, all objects must be declared.
 if __name__ == "__main__":
     app = wx.App()
-    thisFrame.initFrame()
+    MainFrame.initObj()
     app.MainLoop()
 
 
